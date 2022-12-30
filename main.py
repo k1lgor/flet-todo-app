a0c842036f2f4f67f22555f5cdb8d2f475aecb1a
Plamen Ivanov <paco.iwanow@gmail.com>
2022-12-30 14:55:24 +0200

    add light/dark theme

--- main.py
+++ main.py
@@ -5,11 +5,25 @@ from modules.todo import TodoApp
 def main(page: ft.Page):
     page.title = "ToDo App"
     page.horizontal_alignment = "center"
+
+    def theme_changed(e):
+        page.theme_mode = (
+            ft.ThemeMode.DARK
+            if page.theme_mode == ft.ThemeMode.LIGHT
+            else ft.ThemeMode.LIGHT
+        )
+        c.label = (
+            "Light theme" if page.theme_mode == ft.ThemeMode.LIGHT else "Dark theme"
+        )
+        page.update()
+
+    page.theme_mode = ft.ThemeMode.LIGHT
+    c = ft.Switch(label="Light theme", on_change=theme_changed)
     page.update()
 
     app = TodoApp()
 
-    page.add(app)
+    page.add(c, app)
 
 
 ft.app(target=main, view=ft.WEB_BROWSER, port=5000)
